ea30eab6f35968b222b61595ca7bc67c2279120e
Aisosaibi <Aisosaibi.w@gmail.com>
2026-04-17 10:17:29 +0100

    Practice exercise with python

--- discount.py
+++ discount.py
@@ -1,13 +1,13 @@
 total_purchases = int(input("Total spending: "))
 
 if total_purchases > 1000 and total_purchases < 10000:
-    total_purchases = 0.05 * total_purchases
+    total_purchases = (1 - 0.05) * total_purchases
     print(f"Your discounted price is {total_purchases}")
 
 elif total_purchases > 10000 and total_purchases < 50000:
-    total_purchases = 0.10 * total_purchases
+    total_purchases = (1 - 0.10) * total_purchases
     print(f"Your discounted price is {total_purchases}")
 
 elif total_purchases > 50000:
-    total_purchases = 0.20 * total_purchases
+    total_purchases = (1 - 0.20) * total_purchases
     print(f"Your discounted price is {total_purchases}")
