0b30a0dc785345c9078198f24c60d38146df27ea
Aisosaibi <Aisosaibi.w@gmail.com>
2026-04-17 15:29:26 +0100

    Practice with pseudocode and basic nested ifs in python

--- passwordstrength.py
+++ passwordstrength.py
@@ -1,10 +1,17 @@
 """
+PSEUDOCODE FOR PASSWORDSTRENGTH DETERMINER:
+
 Prompt user for password
 Find out the length of password
-If length is < 8: very weak 
-Elif length is == 8: weak 
-If length is > 8: weak 
-If length is < 8: weak    
+If length is > 8: 
+    if length < 16: Output "Strong"
+    else: Output "Very strong"
+
+(I chose this because it encapsulates the if statements by putting all scenarios where 8 is above 8 such that I can refrain from using the and keyword)
+
+Else if length < 8: Output "Very weak"
+Else: "Weak" 
+
 """
 
 
